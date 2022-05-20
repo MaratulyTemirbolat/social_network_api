e6ad0a7b971300f309f3c9a9668cf79efa0678b0
TemirbolatMaratuly <snake2009003@mail.ru>
2022-05-20 15:13:49 +0600

    Model initialization v2.0

--- auths/models.py
+++ auths/models.py
@@ -1,14 +1,36 @@
+from datetime import date
+
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
 )
-from django.utils import timezone
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.core.exceptions import ValidationError
 from django.db.models import QuerySet
 
 from abstracts.models import AbstractDateTime
+
+
+def adult_validation(age: date) -> None:  # noqa
+    ADULT_AGE = 18
+    today: date = date.today()
+    cur_age: int = today.year - age.year - (
+        (today.month, today.day) < (age.month, age.day)
+    )
+    if cur_age < ADULT_AGE:
+        raise ValidationError(
+            "Ваш возраст не может быть меньше 18 лет (функция валидатор)",
+            code='adult_age_error'
+        )
+
+
+def email_lower_case_validation(email: str) -> None:  # noqa
+    if(any(letter.isupper() for letter in email)):
+        raise ValidationError(
+            "Почта не может иметь ни один символ в верхнем регистре",
+            code='lower_case_email_error'
+        )
 
 
 class CustomUserManager(BaseUserManager):  # noqa
@@ -56,8 +78,9 @@ class CustomUser(
     AbstractDateTime
 ):  # noqa
     email = models.EmailField(
-        verbose_name='Почта/Логин',
-        unique=True
+        unique=True,
+        validators=[email_lower_case_validation],
+        verbose_name='Почта/Логин'
     )
     is_active = models.BooleanField(
         'Активность',
@@ -65,17 +88,39 @@ class CustomUser(
     )
     is_staff = models.BooleanField('Статус менеджера', default=False)
     friends = models.ManyToManyField(
-        'self',
+        to='self',
         blank=True,
         null=True
     )
     blocked_users = models.ManyToManyField(
-        "self",
+        to="self",
         blank=True,
         null=True
     )
+    last_seen = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Последний вход"
+    )
+    is_online = models.BooleanField(
+        default=True,
+        verbose_name="Онлайн"
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="Никнейм"
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=255,
+        verbose_name="Url"
+    )
+    birthday = models.DateField(
+        validators=[adult_validation],
+        verbose_name="Дата рождения"
+    )
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username", "birthday"]
 
     objects = CustomUserManager()
 
--- social_network/models.py
+++ social_network/models.py
@@ -12,8 +12,8 @@ class Phone(AbstractDateTime):  # noqa
     owner = models.ForeignKey(
         CustomUser,
         on_delete=models.RESTRICT,
-        verbose_name="Владелец",
-        related_name="phones"
+        related_name="phones",
+        verbose_name="Владелец"
     )
 
 
@@ -23,15 +23,41 @@ class Chat(AbstractDateTime):  # noqa
         default=False
     )
     photo = models.ImageField(
-        upload_to='chat/photos/%Y/%m/%d/',
+        upload_to='photos/chats/%Y/%m/%d/',
         blank=True,
         verbose_name='Миниатюра'
     )
-
+    members = models.ManyToManyField(
+        through="ChatMember",
+        through_fields=("user", "chat"),
+        related_name="joined_chats",
+        blank=True,
+        verbose_name="Члены чата"
+    )
     class Meta:  # noqa
         verbose_name = "Чат"
         verbose_name_plural = "Чаты"
         ordering = ["-datetime_updated"]
+
+
+class ChatMember(models.Model):  # noqa
+    user = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.RESTRICT,
+        related_name="chats",
+        verbose_name="Пользователь"
+    )
+    chat = models.ForeignKey(
+        to=Chat,
+        on_delete=models.RESTRICT,
+        related_name="members",
+        verbose_name="Чат"
+    )
+    chat_name = models.CharField(
+        max_length=150,
+        default=user.username,
+        verbose_name="Никнейм в чате"
+    )
 
 
 class Message(AbstractDateTime):  # noqa
@@ -150,13 +176,15 @@ class Group(AbstractDateTime):  # noqa
     followers = models.ManyToManyField(
         to=CustomUser,
         related_name="groups",
-        blank=True
+        blank=True,
+        verbose_name="Подписчики"
     )
     members_rights = models.ManyToManyField(
         to=CustomUser,
         through="GroupAdministration",
         through_fields=("user", "group"),
-        related_name="group_rights"
+        related_name="group_rights",
+        verbose_name="Позиция в группе"
     )
 
 
@@ -175,4 +203,207 @@ class GroupAdministration(AbstractDateTime):  # noqa
         to=Position,
         on_delete=models.RESTRICT,
         verbose_name="Статус"
+    )
+
+
+class Performer(AbstractDateTime):  # noqa
+    username = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Никнейм"
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        verbose_name="Url"
+    )
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Имя"
+    )
+    surname = models.CharField(
+        max_length=100,
+        verbose_name="Фамилия"
+    )
+
+
+class Playlist(AbstractDateTime):  # noqa
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Название плэйлиста"
+    )
+    photo = models.ImageField(
+        uploaded_to="photos/playlists/%Y/%m/%d",
+        blank=True,
+        verbose_name="Фото плэйлиста"
+    )
+    listeners = models.ManyToManyField(
+        to=CustomUser,
+        related_name="playlists",
+        verbose_name="Слушатели",
+        blank=True
+    )
+
+
+class Music(AbstractDateTime):  # noqa
+    music = models.FileField(
+        upload_to="documents/songs/%Y/%m/%d",
+        verbose_name="Файл песни",
+        unique=True
+    )
+    playlist = models.ForeignKey(
+        to=Playlist,
+        on_delete=models.RESTRICT,
+        related_name="playlist_songs",
+        verbose_name="Плэйлист"
+    )
+    performers = models.ManyToManyField(
+        to=Performer,
+        related_name="performer_songs",
+        verbose_name="Певцы"
+    )
+    users = models.ManyToManyField(
+        to=CustomUser,
+        related_name="user_songs",
+        verbose_name="Пользователи",
+        blank=True
+    )
+
+
+class Video(AbstractDateTime):  # noqa
+
+    video_file = models.FileField(
+        upload_to="documents/videos/%Y/%m/%d",
+        verbose_name="Видео файл"
+    )
+    owner = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.RESTRICT,
+        related_name="owned_videos",
+        verbose_name="Владелец"
+    )
+    keepers = models.ManyToManyField(
+        to=CustomUser,
+        related_name="added_videos",
+        verbose_name="Имеется пользователями"
+    )
+
+
+class News(AbstractDateTime):  # noqa
+    photo = models.ImageField(
+        upload_to="photos/news/%Y/%m/%d",
+        verbose_name="Миниатюра"
+    )
+    content = models.TextField(
+        verbose_name="Контент новости"
+    )
+    group = models.ForeignKey(
+        to=Group,
+        on_delete=models.RESTRICT,
+        blank=True,
+        null=True,
+        related_name="group_news",
+        verbose_name="Создано группой"
+    )
+    author = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.RESTRICT,
+        blank=True,
+        null=True,
+        related_name="published_news",
+        verbose_name="Автор новости"
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.RESTRICT,
+        related_name="news",
+        verbose_name="Категория"
+    )
+    liked_users = models.ManyToManyField(
+        to=CustomUser,
+        related_name="liked_posts",
+        verbose_name="Лайки ползователей",
+        blank=True
+    )
+    tags = models.ManyToManyField(
+        to=Tag,
+        blank=True,
+        related_name="news",
+        verbose_name="Тэги"
+    )
+
+
+class Comment(AbstractDateTime):  # noqa
+    content = models.TextField(
+        verbose_name="Контент"
+    )
+    likes = models.ManyToManyField(
+        to=CustomUser,
+        related_name="liked_comments",
+        blank=True,
+        verbose_name="Лайки пользователей"
+    )
+    commentator = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.RESTRICT,
+        related_name="comments",
+        verbose_name="Владелец комментария"
+    )
+    news = models.ForeignKey(
+        to=News,
+        on_delete=models.RESTRICT,
+        related_name="comments",
+        verbose_name="Новость"
+    )
+
+
+class Country(AbstractDateTime):  # noqa
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Название страны"
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        verbose_name="Url"
+    )
+
+
+class City(AbstractDateTime):  # noqa
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименованеи города"
+    )
+    country = models.ForeignKey(
+        to=Country,
+        on_delete=models.RESTRICT,
+        related_name="attached_cities",
+        verbose_name="Страна"
+    )
+
+
+class ProfilePhoto(AbstractDateTime):  # noqa
+    photo = models.ImageField(
+        upload_to="photos/profile_photos/%Y/%m/%d",
+        verbose_name="Фото профиля"
+    )
+    owner = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.RESTRICT,
+        related_name="profile_photos"
+    )
+    likes_number = models.IntegerField(
+        verbose_name="Количество лайков",
+        default=0
+    )
+    city = models.ForeignKey(
+        to=City,
+        on_delete=models.RESTRICT,
+        related_name="profile_photos",
+        verbose_name="Город, где сделано фото"
+    )
+    is_title = models.BooleanField(
+        default=False,
+        verbose_name="Аватарка"
     )
